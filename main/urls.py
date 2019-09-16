c6acf38b52462c0e93603c15858271de234b58dc
Samuel Scott Jennings <a1654095@adelaide.edu.au>
2019-09-16 16:18:47 +0930

    remove mapping urls

--- main/urls.py
+++ main/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('resources/',views.resources,name='resources'),
     path('about/',views.AboutView.as_view(),name='about'),
     path('contact/',views.ContactView.as_view(),name='contact'),
-    path('thermoglobe/',views.DatabaseView.as_view(),name='database'),
     path('thermoglobe/site/<site_id>/<site_name>/', views.SiteView.as_view(), name='site'),
     path('upload/',views.UploadView.as_view(),name='upload'),
     path('upload/confirm/',views.ConfirmUploadView.as_view(),name='confirm_upload'),
-    path('thermoglobe/filter', views.filter_data,name='filter_data')
+    # path('thermoglobe/download', views.download_data,name='download_data'),
+
     # path('confirm/',confirm,name='confirm'),
 ]
