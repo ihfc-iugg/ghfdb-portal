2332fe23e247f2f9125603930ef783065dce6e7c
Samuel Scott Jennings <a1654095@adelaide.edu.au>
2019-10-14 11:08:41 +1030

    update urls

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('',views.HomeView.as_view(),name='home'),
-    path('resources/',views.resources,name='resources'),
+    path('resources/',views.ResourcesView.as_view(),name='resources'),
     path('about/',views.AboutView.as_view(),name='about'),
     path('contact/',views.ContactView.as_view(),name='contact'),
-    path('thermoglobe/site/<site_id>/<site_name>/', views.SiteView.as_view(), name='site'),
+    path('thermoglobe/site/<pk>/', views.SiteView.as_view(), name='site'),
     path('upload/',views.UploadView.as_view(),name='upload'),
     path('upload/confirm/',views.ConfirmUploadView.as_view(),name='confirm_upload'),
     # path('thermoglobe/download', views.download_data,name='download_data'),
