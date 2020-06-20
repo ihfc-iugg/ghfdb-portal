5747d2104b666f303a4983e030d2b5213e9c1721
Samuel Scott Jennings <a1654095@adelaide.edu.au>
2020-06-20 12:14:22 +0930

    debug_toolbar

--- heatflow/urls.py
+++ heatflow/urls.py
@@ -30,13 +30,13 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    # import debug_toolbar
-    # urlpatterns = [
+    import debug_toolbar
+    urlpatterns = [
 
-    #     path('__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
 
 
-    # ] + urlpatterns
+    ] + urlpatterns
 
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
