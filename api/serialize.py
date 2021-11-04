9935308311fbfa8f236260838cf75ca8b987acc2
SSJenny90 <samuel.scott.jennings@gmail.com>
2021-11-05 00:46:33 +1030

    update api serializers

--- api/serialize.py
+++ api/serialize.py
@@ -3,11 +3,29 @@ from rest_framework import serializers
 from thermoglobe import models
 from publications import models as pub_models
 
-class Site(serializers.HyperlinkedModelSerializer):
+
+class SimpleSite(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    data_counts = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Site
-        # fields = "__all__"
-        exclude = ['reference','geom','slug']
+        fields = ['url','id','site_name','latitude','longitude','elevation','description','data_counts']
+        # exclude = ['reference','slug','geom','continent','country','political','province','sea','crustal_thickness','seamount_distance','outcrop_distance']
+
+    def get_data_counts(self,obj):
+        return obj.data_counts()
+
+class Site(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    data_counts = serializers.SerializerMethodField()
+    class Meta:
+        model = models.Site
+        exclude = ['reference','slug',]
+        read_only_fields = ['continent','country','political','province','sea','crustal_thickness','seamount_distance','outcrop_distance']
+
+    def get_data_counts(self,obj):
+        return obj.data_counts()
 
 class Publication(serializers.HyperlinkedModelSerializer):
 
@@ -16,35 +34,58 @@ class Publication(serializers.HyperlinkedModelSerializer):
         exclude = ['verified_by','bibtex','pdf',]
 
 class Interval(serializers.HyperlinkedModelSerializer):
-    site = Site(read_only=True)
-    reference = Publication(read_only=True)
+    site = SimpleSite(read_only=True)
+    heat_flow = serializers.FloatField()
+    heat_flow_uncertainty = serializers.FloatField()
+    gradient = serializers.FloatField()
+    gradient_uncertainty = serializers.FloatField()
+
+    # _heat_flow = HeatFlow()
+    _corrected = serializers.SerializerMethodField()
+    _uncorrected = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Interval
-        exclude = ['global_by']
+        # fields = ['site','reference','heat_flow','gradient','_heat_flow']
+        exclude = ['global_by','heat_flow_corrected','heat_flow_corrected_uncertainty','gradient_corrected','gradient_corrected_uncertainty','heat_flow_uncorrected','heat_flow_uncorrected_uncertainty','gradient_uncorrected','gradient_uncorrected_uncertainty']
+
+    def get__corrected(self, obj):
+        return dict(
+            heat_flow=obj.heat_flow_corrected,
+            heat_flow_uncertainty=obj.heat_flow_corrected_uncertainty,
+            gradient=obj.gradient_corrected,
+            gradient_uncertainty=obj.gradient_corrected_uncertainty,
+        )
+
+    def get__uncorrected(self, obj):
+        return dict(
+            heat_flow=obj.heat_flow_uncorrected,
+            heat_flow_uncertainty=obj.heat_flow_uncorrected_uncertainty,
+            gradient=obj.gradient_uncorrected,
+            gradient_uncertainty=obj.gradient_uncorrected_uncertainty,
+        )
 
 class Conductivity(serializers.HyperlinkedModelSerializer):
-    site = Site(read_only=True)
-    reference = Publication(read_only=True)
+    site = SimpleSite(read_only=True)
 
     class Meta:
         model = models.Conductivity
-        fields = "__all__"
+        exclude = ['date_added']
 
 class HeatProduction(serializers.HyperlinkedModelSerializer):
-    site = Site(read_only=True)
-    reference = Publication(read_only=True)
+    site = SimpleSite(read_only=True)
 
     class Meta:
         model = models.HeatProduction
-        fields = "__all__"
+        exclude = ['date_added']
+
 
 class Temperature(serializers.HyperlinkedModelSerializer):
-    site = Site(read_only=True)
-    reference = Publication(read_only=True)
+    site = SimpleSite(read_only=True)
     
     class Meta:
         model = models.Temperature
-        fields = "__all__"
+        exclude = ['date_added']
+
 
 
--- api/views.py
+++ api/views.py
@@ -6,58 +6,60 @@ from thermoglobe.models import Site, Interval, Conductivity, HeatProduction, Tem
 from publications.models import Publication
 from api.paginators import DataTablesPaginator
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework_gis.filters import DistanceToPointOrderingFilter, DistanceToPointFilter
+# from rest_framework_gis.schema import GeoFeatureAutoSchema
 
-class SiteViewSet(viewsets.ModelViewSet):
+class SiteViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of ThermoGlobe sites."""
     queryset = Site.objects.all()
     serializer_class = serialize.Site
+    distance_filter_field = 'geom'
+    distance_ordering_filter_field = 'geom'
+    filterset_fields = ['reference',]
+    filter_backends = (DistanceToPointFilter,DistanceToPointOrderingFilter,DjangoFilterBackend)
 
-class PublicationViewSet(viewsets.ModelViewSet):
+class PublicationViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of ThermoGlobe publications."""
     queryset = Publication.objects.all()
     serializer_class = serialize.Publication
 
-class HeatFlowViewSet(viewsets.ModelViewSet):
+class HeatFlowViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of heat flow intervals."""
-    queryset = Interval.heat_flow.all()
+    queryset = Interval.heat_flow.select_related('site','reference')
     serializer_class = serialize.Interval
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = DataTablesPaginator
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['reference']
+    filterset_fields = ['reference','site']
 
-class GradientViewSet(viewsets.ModelViewSet):
+class GradientViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of thermal gradient intervals."""
     queryset = Interval.gradient.all()
     serializer_class = serialize.Interval
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = DataTablesPaginator
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['reference']
+    filterset_fields = ['reference','site']
 
-class ConductivityViewSet(viewsets.ModelViewSet):
+class ConductivityViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of thermal conductivity measurements."""
-    queryset = Conductivity.objects.all()
+    queryset = Conductivity.objects.select_related('reference','site')
     serializer_class = serialize.Conductivity
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = DataTablesPaginator
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['reference']
+    filterset_fields = ['site','reference']
 
-class HeatProductionViewSet(viewsets.ModelViewSet):
+class HeatProductionViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of heat production measurements."""
     queryset = HeatProduction.objects.all()
     serializer_class = serialize.HeatProduction
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = DataTablesPaginator
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['reference']
+    filterset_fields = ['reference','site']
 
-class TemperatureViewSet(viewsets.ModelViewSet):
+class TemperatureViewSet(viewsets.ReadOnlyModelViewSet):
     """API endpoint to request a set of temperature measurements."""
     queryset = Temperature.objects.all()
     serializer_class = serialize.Temperature
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = DataTablesPaginator
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['reference']
+    filterset_fields = ['reference','site']
