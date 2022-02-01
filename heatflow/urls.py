509944ec0aca359fc5a3893717f48129d5b881f9
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-02 00:36:51 +1030

    reodredered urls

--- heatflow/urls.py
+++ heatflow/urls.py
@@ -5,20 +5,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls import url
+from django.conf.urls.i18n import i18n_patterns
 
 
 
-
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("sitemap.xml", sitemap, {"sitemaps": {"cmspages": CMSSitemap}}),
+    path("accounts/", include("allauth.urls")),
+    path('', include('users.urls')),
+
     path('taggit_autosuggest/', include('taggit_autosuggest.urls')),
-    path("admin/", admin.site.urls), 
     path('', include('thermoglobe.urls')),
     path('publications/', include('publications.urls')),
-    path("api/", include("api.urls")),
-    path("", include("cms.urls")),
-]
+    path('comments/', include('django_comments.urls')),
+    path('', include('dashboard.urls')),
 
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
+    path("admin/", admin.site.urls), 
+    path('', include("cms.urls")),
+    
+)
+
+    
+urlpatterns += [
+    path("api/", include("api.urls")),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
