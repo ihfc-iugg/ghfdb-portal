bdd978e0f80299d9579ce0ee46e723ac428489ee
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-02 11:51:30 +1030

    new map filter and download form

--- thermoglobe/forms.py
+++ thermoglobe/forms.py
@@ -1,21 +1,59 @@
 from django import forms
 from . import models, import_choices as choices
-from betterforms.forms import BetterModelForm
-from betterforms.multiform import  MultiModelForm
 from mapping import forms as map_forms
 
-class DownloadForm(forms.Form):
+from django import forms
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit, Div, Row, Column, Field, HTML, Button
+from thermoglobe.models import Site
+
+class MapFilterForm(forms.Form):
+
+    site_name__icontains = forms.CharField(required=False, label='Site Name')
+    latitude__gt = forms.FloatField(required=False, label='Latitude')
+    latitude__lt = forms.FloatField(required=False, label='<div class="invisible">Latitude</div>')
+    longitude__gt = forms.FloatField(required=False, label='Longitude')
+    longitude__lt = forms.FloatField(required=False, label='<div class="invisible">Longitude</div>')
+    elevation__gt = forms.FloatField(required=False, label='Elevation (m)')
+    elevation__lt = forms.FloatField(required=False, label='<div class="invisible">Elevation</div>')
+
+    class Meta:
+        model=Site
+        # fields = ['site_name','latitude__gt', 'latitude__gt']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'GET'
+        self.helper.form_show_labels = True
+        self.helper.form_id = 'map-filter-form'
+        self.helper.layout = Layout(
+            Field('site_name__icontains', css_class='mb-2'),
+            Row(
+                Column(Field('latitude__gt', placeholder='From', css_class='form-control'), 
+                        css_class='form-group col-md-6'),
+                Column(Field('latitude__lt', placeholder='To', css_class='form-control'), 
+                            css_class='form-group col-md-6'),
+            css_class='align-bottom',
+            ),
+            Row(Column(Field('longitude__gt', placeholder='From', css_class='form-control'), css_class='form-group col-md-6'),
+                Column(Field('longitude__lt', placeholder='To', css_class='form-control'), css_class='form-group col-md-6')),
+            Row(Column(Field('elevation__gt', placeholder='Greater than', css_class='form-control'), css_class='form-group col-md-6'),
+                Column(Field('elevation__lt', placeholder='Less than', css_class='form-control'), css_class='form-group col-md-6')),
+            ButtonHolder(
+                Button('button', 'Search', onclick='updateMap()', css_class='button mt-2'),
+            )
+            )
+class DownloadWithChoicesForm(forms.Form):
     download_type = forms.ChoiceField(
         label="Download Type",
         help_text='The level of detail required in the downloaded files. Higher levels will result in larger download sizes.',
         required=True,
-        choices=[
-            ('basic','Basic'),
-            ('standard','Standard'),
-            ('detailed','Detailed'),
-        ]
-    )
-    # your_name = forms.CharField(label='Your name', max_length=100)
+        choices=[('basic','Basic'),
+                ('standard','Standard'),
+                ('detailed','Detailed')])
+
     data_select = forms.MultipleChoiceField(
         label='Data Select',
         help_text='Select one or more of the data types below. Download will contain a csv file for each selected data type.',
@@ -32,16 +70,52 @@ class DownloadForm(forms.Form):
             style="height: 100%",
          ))
     )
+
     class Meta:
         fields = ['download_type','data_select']
 
-class SiteForm(BetterModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'GET'
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+                Field('download_type', css_class='mb-2'),
+                Field('data_select', css_class='mb-2'),
+            ButtonHolder(
+                Submit('submit', 'Download', css_class='button mt-2')
+            )
+        )
+
+class DownloadBasicForm(forms.Form):
+    download_type = forms.ChoiceField(
+        label="Download Type",
+        help_text='Standard: simple heat flow and thermal gradient estimates. Detailed: Includes corrected and uncorrected estimates.',
+        required=False,
+        choices=[   ('standard','Standard'),
+                    ('detailed','Detailed')])
+
+    class Meta:
+        fields = ['download_type']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.form_show_labels = True
+        self.helper.layout = Layout(
+                Field('download_type', css_class='mb-2'),
+                ButtonHolder(
+                Submit('submit', 'Download', css_class='button mt-2')
+            ))
+
+class SiteForm(ModelForm):
 
     class Meta:
         model = models.Site
         fields = choices.SITE_FIELDS
 
-class HeatFlowForm(BetterModelForm):
+class HeatFlowForm(ModelForm):
     class Meta:
         model = models.Interval
         fields = [            
@@ -62,7 +136,7 @@ class HeatFlowForm(BetterModelForm):
             'conductivity_method',
             ]
 
-class CorrectionForm(BetterModelForm):
+class CorrectionForm(ModelForm):
     class Meta:
         model = models.Correction
         fields = [            
@@ -76,28 +150,28 @@ class CorrectionForm(BetterModelForm):
             'other_type',
             ]
 
-class ConductivityForm(BetterModelForm):
+class ConductivityForm(ModelForm):
     conductivity = forms.FloatField(required=True,)
 
     class Meta:
         model = models.Conductivity
         fields = choices.CONDUCTIVITY_FIELDS
 
-class HeatGenForm(BetterModelForm):
+class HeatGenForm(ModelForm):
     heat_production = forms.FloatField(required=True,)
 
     class Meta:
         model = models.HeatProduction
         fields = choices.HEAT_GEN_FIELDS
 
-class TemperatureForm(BetterModelForm):
+class TemperatureForm(ModelForm):
     temperature = forms.FloatField(required=True,)
 
     class Meta:
         model = models.Temperature
         fields = choices.TEMPERATURE_FIELDS
 
-class SiteMultiForm(MultiModelForm):
+class SiteMultiForm(ModelForm):
     form_classes = {
         'Site': SiteForm,
         'Country': map_forms.CountryForm,
