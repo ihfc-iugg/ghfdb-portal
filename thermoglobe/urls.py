420ce8e8190131a841a14e9308650c0cbaa4c44e
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-02 11:52:21 +1030

    add data_url

--- thermoglobe/urls.py
+++ thermoglobe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from thermoglobe import views
-# app_name = 'thermoglobe'
+app_name = 'thermoglobe'
 
 
 urlpatterns = [
     path('world-map/',views.WorldMap.as_view(),name='world_map'),
+    path('world-map/data/', views.quick_sites),
     path('sites/<pk>/', views.SiteView.as_view(), name='site'),
     # path('sites/<slug>/', views.SiteView.as_view(), name='site'),
     # path('publications/', views.PublicationListView.as_view(),name='publication_list'),
