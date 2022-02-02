11d528482707886c252b0d8d845495735b794745
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-02 11:53:08 +1030

    updated

--- thermoglobe/static/thermoglobe/js/table.js
+++ thermoglobe/static/thermoglobe/js/table.js
@@ -1,74 +1,53 @@
+$.extend( $.fn.dataTable.defaults, {
+  dom: `<"top"lf>
+          rt
+        <"bottom"ip>
+        <"clear">`,
+  responsive: true,
+  serverSide: true,
+  pageLength: 50,
+} );
+
 $.fn.create_table = function(options) {
-
-    var fields = [];
-    $(options['fields']).each(function (ind, field) {
-      if (typeof(field) == "object" ) {
-        fields.push(field)
-      } else {
-        fields.push({"data": field})       
-      }
-    })
-
-    this.DataTable( {
-        dom: '<"top"il>rt<"bottom"p><"clear">',
-        ordering: false,
-        responsive: true,
-        processing: true,
-        serverSide: true,
-        columns: fields,
-        ajax: {
-          type: "GET",
-          url: options['url'],
-          dataSrc: 'results',
-          dataFilter: function(data) {
-              var json = jQuery.parseJSON( data );
-              json.recordsTotal = json.count;
-              json.recordsFiltered = json.count;
-              return JSON.stringify( json );
-          },
-        },
-    });
-  return this;
+  this.DataTable( {
+      columnDefs: [{targets: [0], render: $.fn.dataTable.render.href()}],
+  });
+return this;
 };
+
 
 $.fn.dataTable.render.href = function ( href ) {
   return function ( data, type, row ) {
-      return '<a href='+href+row.site.id+'>'+data+'</a>'
+      return '<a href='+row.site.web_url+'>'+data+'</a>'
   }
 };
 
-$('tbody tr').hover(function () {
-  var data = site.row( this ).data();
-  console.log(data)
-  var coordinates = new L.LatLng(data.latitude,data.longitude);
-  marker.setLatLng(coordinates);
-  marker.addTo(map)
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+  beforeSend: function (xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+          xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+  }
 });
-
-$('#dataTable').on( 'mouseenter', 'tbody tr', function () {
-  var data = table.row( this ).data();
-  var coordinates = new L.LatLng(data[2],data[3]);
-  tmpMarker.setLatLng(coordinates);
-  tmpMarker.addTo(map)
-} );
-
-$('#dataTable').on( 'click', 'tbody tr', function () {
-  var data = table.cells( this, '' ).render( 'display' );
-
-  var lat, lon = none,none;
-
-  var popup = '<table>'
-  table.columns().header().each(function(column, index){
-    if (column.innerHTML.toLowerCase() === 'latitude') {
-      lat = data[index]
-    } else if (column.innerHTML.toLowerCase() === 'longitude')  {
-      lon = data[index]
-    }
-    popup += `<tr><td>${column.innerHTML}:</td><td>${data[index]}</td></tr>`
-  })
-  var coordinates = new L.LatLng(lat, lon);
-  new L.marker(coordinates).addTo(map).bindPopup(popup + '</table>');
-
-} );
-
-
