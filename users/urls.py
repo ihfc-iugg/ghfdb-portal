532b5756d758fcaefb04868758b4236c82f1963c
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-02-12 20:29:29 +1030

    significatnr efactoring

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from users.views import why_register
-
-app_name = 'users'
-urlpatterns = [
-    path('why-register/', why_register, name='why_register'),
-]
--- users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-def why_register(request):
-    return render(request, 'dashboard/why_register.html')
-
