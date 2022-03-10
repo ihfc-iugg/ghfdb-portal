9d505472d7381fd51115425aae0a8af04bacffe3
SSJenny90 <samuel.scott.jennings@gmail.com>
2022-03-10 14:37:27 +1030

    housekeeping

--- api/paginators.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import LimitOffsetPagination
-
-
-class DataTablesPaginator(LimitOffsetPagination):
-    max_limit = 100
-    offset_query_param = 'start'
-    limit_query_param = 'length'
