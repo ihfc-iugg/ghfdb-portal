5fadfb712ce7f44a5598134421c4cde5a798269e
Sam <samuel.scott.jennings@gmail.com>
2022-07-27 11:10:30 +0200

    refactoring

--- thermoglobe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from thermoglobe import views
-app_name = 'thermoglobe'
-
-urlpatterns = [
-    path('sites/',views.WorldMap.as_view(),name='world_map'),
-    path('sites/<pk>/', views.SiteView.as_view(), name='site'), 
-]
