2f36a52b9fb4731ec9b95f9041e906ed59d4dfa8
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-09-28 11:07:03 +0200

    refactored to core/user

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from dashboard.views import dashboard, user_settings
-
-app_name = 'dashboard'
-urlpatterns = [
-    path('dashboard/', dashboard, name='dashboard'),
-    path('settings/', user_settings, name='settings'),
-
-]
