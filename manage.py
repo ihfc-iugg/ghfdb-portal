38ddb14bd33a69304294ce21557f9ea4c970b5dd
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-10-04 09:43:03 +0200

    tweaks to workspace and git settings

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 # print(sys.version)
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'global_heat_flow.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
