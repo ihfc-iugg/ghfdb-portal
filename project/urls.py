1aa4e862462626ce2565337ba9770a6959b05849
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-10-04 09:41:29 +0200

    renamed project folder to "project"

--- project/urls.py
+++ project/urls.py
@@ -8,15 +8,15 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
     path('', include('main.urls')),
+    path('', include('user.urls')),
+    path('', include('core.urls')),
+    path('viewer.app', include('kepler.urls')),
     path('publications/', include('publications.urls')),
     path('datacite/', include('datacite.urls')),
-    path('', include('user.urls')),
     path('earth_science', include('earth_science.urls')),
     path("invitations/", include('invitations.urls', namespace='invitations')),
     path('translate/', include('rosetta.urls')),
     path('admin/', include('smuggler.urls')),  # before admin url patterns!
-    path('', include('kepler.urls')),
-    path('', include('core.urls')),
 )
 
     
