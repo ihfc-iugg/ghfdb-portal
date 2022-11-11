b2b0033a2f9e35a647ff4c34c865c74e77d20739
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-11 11:13:08 +0100

    refactored into geoluminate

--- main/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MainConfig(AppConfig):
-    name = 'main'
-    verbose_name = 'Main'
-
-    def ready(self):
-        from . import signals
--- main/models.py
+++
@@ -1,6 +0,0 @@
-from invitations.base_invitation import AbstractBaseInvitation
-
-class Invitations(AbstractBaseInvitation):
-    pass
-
-
