2275d748116003164ab89db715a13b02c5ca4ae7
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 10:55:21 +0100

    updated import procedure

--- database/widgets.py
+++ database/widgets.py
@@ -1,55 +1,100 @@
-from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
+from import_export.widgets import ForeignKeyWidget
 from django.utils.translation import ugettext as _
-from decimal import Decimal
-
-def get_non_relational_fields(model,row,exclude=[]):
-    return [f for f in model._meta.concrete_fields if row.get(f.name) and f.name not in exclude and not f.is_relation]
+from django.contrib.gis.geos import Point
+from django.forms import modelform_factory
+from import_export.fields import Field
+from geoluminate.widgets import ChoiceManyToMany
 
 
 class SiteWidget(ForeignKeyWidget):
 
     def clean(self, value, row=None, *args, **kwargs):
 
-        defaults = {f.name: row.get(f.name) for f in get_non_relational_fields(self.model, row)}
+        row['geom'] = Point(row['lng'], row['lat'], srid=row.get('srid', 4326))
+        # row['id'] = row['ID']
+        form, valid = self.get_form(row)
 
-        qs = self.get_queryset(value, row)
+        geom = form.cleaned_data.pop('geom')
 
-        for k in ['name','lat','lng']:
-            defaults.pop(k, None)
-
-        # if qs.exists():
-        instance, created = qs.update_or_create(
-            name = row['name'],
-            lat = round(row['lat'],5),
-            lng = round(row['lng'],5),
-            defaults = defaults,
+        del form.cleaned_data['references']
+        # del form.cleaned_data['id']
+        # if row['id'] == 8:
+        # x = 8
+        instance, created = self.model.objects.update_or_create(
+            geom=geom,
+            defaults=form.cleaned_data,
         )
+
         return instance
 
+    def get_form(self, row):
+        form = modelform_factory(self.model, fields="__all__")(row)
+        return form, form.is_valid()
 
-class CorrectionsWidget(ManyToManyWidget):
+
+class CorrectionsWidget(ChoiceManyToMany):
 
     def clean(self, value, row=None, *args, **kwargs):
-
+        """Create a list of `Choice` ids."""
         corrs = []
         for k, v in row.items():
-            if v is None:
+            if v is None or not k.startswith('corr_'):
                 continue
-            if k.startswith('corr_') and v.lower() == 'yes':
-                corrs.append(
-                    self.model.objects.get(
-                            id = k.split('_')[1],
-                            ))
+
+            # get the code from the row key. In the csv file,
+            # correction values look like `corr_HP_flag` so split
+            # by `_` and take the second item
+            code = k.split('_')[1]
+
+            # start building a dict that will represent the
+            # intermerdiate model between `Interval` and `Choice`.
+            # e.g. CorrectionIntervalThrough.
+            data = {
+                "correction": self.get_queryset(v, row).get(
+                    **{self.field: k.split('_')[1]})
+            }
+
+            try:
+                # given value is a number so add it to the correct
+                # fields
+                data.update(applied='yes', value=float(v))
+            except ValueError:
+                # the given value is a string so update the applied
+                # field only
+                data.update(applied=v.lower())
+
+            # append it to the corrs list
+            corrs.append(data)
+
         return corrs
+
+
+class CorrectionField(Field):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            widget=CorrectionsWidget(type='corrections'),
+            *args, **kwargs)
+
+    def save(self, obj, data, is_m2m=False, **kwargs):
+        """
+        If this field is not declared readonly, the object's attribute will
+        be set to the value returned by :meth:`~import_export.fields.Field.clean`.
+        """
+        if not self.readonly:
+            cleaned = self.clean(data, **kwargs)
+            if cleaned is not None or self.saves_null_values:
+                Intermediate = getattr(obj, self.attribute).through
+                for corr in cleaned:
+                    Intermediate(interval=obj, **corr).save()
 
 
 class ReferenceWidget(ForeignKeyWidget):
 
     def clean(self, value, row=None, *args, **kwargs):
-        val = value
-        if val:
-            instance, created = self.get_queryset(value, row, *args, **kwargs).get_or_create(**{self.field: val})
+        if value:
+            instance, created = self.get_queryset(
+                value, row, *args, **kwargs).get_or_create(**{self.field: value})
             return instance
         else:
             return None
-
