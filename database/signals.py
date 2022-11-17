3450c00695f401f53374823eeb3000d9645e5a4e
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 10:56:10 +0100

    update parent reference on child save

--- database/signals.py
+++ database/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import m2m_changed, pre_save, post_save
 from django.dispatch import receiver
-from database.models import Site, Interval
+from database.models import Interval
 
 
 @receiver(post_save, sender=Interval)
@@ -8,7 +8,8 @@ def interval_changed(sender, instance, **kwargs):
 
     # Preliminary test to update Site instance after modifying an Interval
 
-    # Current updates the site acquisition date to the latest acquired Interval and adds the interval reference to the site object
+    # Current updates the site acquisition date to the latest acquired
+    # Interval and adds the interval reference to the site object
     site = instance.site
     most_recent = site.intervals.order_by('-q_acq').first()
 
