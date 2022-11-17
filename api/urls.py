252d19dd6af2d5cef9ab0bb6e8fb3d1d6c35e918
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 10:20:20 +0100

    refactoring

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('v1/', include("api.v1.urls")),
-    path('', lambda request: redirect('/api/v1', permanent=False))
-]
