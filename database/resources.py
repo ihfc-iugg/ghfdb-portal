fb079877174752602ac2a5eec13d6c46c45ee8c5
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 10:55:21 +0100

    updated import procedure

--- database/resources.py
+++ database/resources.py
@@ -1,11 +1,17 @@
-from database.models import Site, Interval, Choice
+from database.models import HeatFlow, Interval, Choice
 from import_export import resources
 from django.utils.html import mark_safe
 from import_export.instance_loaders import ModelInstanceLoader
-from core.resources import ResourceMixin
-from .widgets import ReferenceWidget, SiteWidget, CorrectionsWidget
-from publications.models import Publication
+from geoluminate.resources import ResourceMixin
+from .widgets import ReferenceWidget, SiteWidget, CorrectionsWidget, CorrectionField
+from literature.models import Publication
 from import_export.fields import Field
+
+
+"""Resource classes are derived from `django-import-export` which allows
+site administrators to import or export data into the admin site
+"""
+
 
 class CustomLoader(ModelInstanceLoader):
 
@@ -27,70 +33,66 @@ class CustomLoader(ModelInstanceLoader):
         except self.resource._meta.model.DoesNotExist:
             return None
 
+
 class SiteResource(ResourceMixin):
 
     class Meta:
-        model = Site
-        exclude = ['continent','country','political','province','ocean','plate','slug','geom','date_added','id'] + [f.name for f in model._meta.related_objects]
-        import_id_fields = ['name','lat','lng']
-        # instance_loader_class = CustomLoader
+        model = HeatFlow
+        exclude = [
+            'slug',
+            'date_added',
+            'id'] + [f.name for f in model._meta.related_objects]
+        import_id_fields = ['name', 'lat', 'lng']
         widgets = {
-                'q_acq': {'format': '%Y-%m'},
-                }
+            'q_acq': {'format': '%Y-%m'},
+        }
         # skip_diff=True
         # use_bulk = True
         # force_init_instance = True
-
 
     def before_import_row(self, row, row_number=None, **kwargs):
         row['id'] = row['ID']
         pass
 
+
 class IntervalResource(ResourceMixin):
 
     site = Field(attribute='site',
-        widget = SiteWidget(Site)
-        )
-    # corrections = Field(attribute='corrections',
-    #     widget = CorrectionsWidget(Correction)
-    #     )
-
+                 widget=SiteWidget(HeatFlow)
+                 )
+    corrections = CorrectionField(attribute='corrections')
     reference_id = Field(attribute='reference',
-        column_name = 'Ref_1', 
-        widget = ReferenceWidget(Publication, field='label')
-        )
-
+                         column_name='Ref_1',
+                         widget=ReferenceWidget(Publication, field='label')
+                         )
 
     class Meta:
         model = Interval
-        exclude = ['date_added']
+        exclude = ['date_added', 'last_modified']
         import_id_fields = ['historic_id']
         force_init_instance = True
-        # raise_errors=True
         skip_unchanged = False
-        report_skipped= True
-        skip_diff = True
+        report_skipped = True
+        # skip_diff = True
         clean_model_instances = True
         widgets = {
-                'q_acq': {'format': '%Y-%m-%d'},
-                }
+            'q_acq': {'format': '%Y-%m-%d'},
+        }
 
     def before_import_row(self, row, row_number=None, **kwargs):
         row['reference_id'] = row.get('Ref_1')
         row['site'] = ''
         row['historic_id'] = row['ID']
         row['corrections'] = ""
+
         if not row['qc']:
             row['qc'] = row['q']
             row['childcomp'] = True
 
-        # converts choice field inputs to lower case for consisteny
+        # converts choice field inputs to lower case for consistency
         row = self.choice_fields_to_lowercase(row)
 
         row = self.clean_date(row)
-
-        pass
-
 
     def clean_date(self, row):
         if row.get('q_acq'):
@@ -108,34 +110,10 @@ class IntervalResource(ResourceMixin):
 
     def choice_fields_to_lowercase(self, row):
         """Convert choice field inputs to lower case for consistency """
-        choice_fields = [f.name for f in self.Meta.model._meta.fields if f.choices]
+        choice_fields = [
+            f.name for f in self.Meta.model._meta.fields if f.choices]
 
         for f in choice_fields:
             if row.get(f):
                 row[f] = row[f].lower()
         return row
-
-
-    # def before_import_row(self, row, row_number=None, **kwargs):
-    #     for item in row['corrections']:
-    #         if item[1]:
-    #             item[0].save()
-    #     row['corrections'] = [i[0] for i in row['corrections']]
-    #     return super().before_import_row(row, row_number, **kwargs)
-
-    #     def get_instance(row, corr_type, value):
-    #         interval = Interval.objects.get(id=row.get('id'))
-    #         if row.get(corr_type+'_flag'):
-    #             if row.get(corr_type) != "":
-    #                 return interval.corrections.create(type=value, value=row.get(corr_type))
-    #             else:
-    #                 return interval.corrections.create(type=value, interval=interval)
-    #             # corrections.append(Correction(type='CLIM', value=val))
-
-    #     row['corrections'] = []
-    #     for value, corr_type in types:
-    #         x = get_instance(row, corr_type, value)
-    #         if x:
-    #             row['corrections'].append(x)
-      
-    #     return super().before_import_row(row, row_number, **kwargs)
