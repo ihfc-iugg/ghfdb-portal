43e85657ebe789742546e835c42057a2e0a3369a
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-24 19:37:33 +0100

    refactoring

---
+++ database/__init__.py
@@ -0,0 +1,3 @@
+"""The database package contains all modules related to the creation of
+schema specific to the Global Heat Flow Database. This includes database
+models, administration classes, enumerators (choices), custom widgets, etc..."""
