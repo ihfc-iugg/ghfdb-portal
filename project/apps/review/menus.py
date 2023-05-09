92a46e40fd738e1afe563885b7c76994ced515e5
Sam <samuel.scott.jennings@gmail.com>
2023-05-09 14:16:17 +0200

    housekeeping

--- project/apps/review/menus.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
-from simple_menu import Menu, MenuItem
-
-# Menu.add_item('toolbar', MenuItem(_("Review"),
-#                                   reverse("review:list"),
-#                                   #   weight=10,
-#                                   icon="fa-pencil-ruler"))
--- project/apps/thermal_data/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-class ThermalDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'thermal_data'
-    verbose_name = _('Thermal Data')
-    verbose_name_plural = _('Thermal Data')
