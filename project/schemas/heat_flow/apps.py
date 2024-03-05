961eeeb11be04c5829cd0b1aedb46c9e57cb6d87
Sam Jennings <samuel.scott.jennings@gmail.com>
2024-03-05 09:43:25 +0000

    update apps.py

--- project/schemas/heat_flow/apps.py
+++ project/schemas/heat_flow/apps.py
@@ -2,9 +2,12 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class HeatFlowConfig(AppConfig):
-    """Config for the Global Heat Flow Database application"""
+class HeatFlowSchemaConfig(AppConfig):
+    """Config for heat flow schema"""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "heat_flow"
     verbose_name = _("Heat Flow")
+
+    # HeatFlow = {"list_display": ["q", "q_uncertainty"]}
+    # HeatFlowChild = {}
