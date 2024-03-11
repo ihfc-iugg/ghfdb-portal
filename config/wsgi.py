949c448aa063e3d61bd9a44fbe57c4c95f49ff4b
Sam <samuel.scott.jennings@gmail.com>
2024-03-12 00:24:44 +0100

    update config

--- config/wsgi.py
+++ config/wsgi.py
@@ -5,8 +5,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
