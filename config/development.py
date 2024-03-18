969fb8830846cfa787d2c3ebd20b362eca06d70b
Sam <samuel.scott.jennings@gmail.com>
2024-03-18 14:10:54 +0100

    update config

--- config/development.py
+++ config/development.py
@@ -4,9 +4,10 @@ from .common import *
 from .geoluminate import *
 
 SHOW_DEBUG_TOOLBAR = False
-
+DEBUG = False
 geoluminate.setup(development=True)
 # geoluminate.setup(development=False)
+DEBUG = False
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 # removing validators for local development
@@ -20,3 +21,12 @@ ACCOUNT_EMAIL_VERIFICATION = "optional"
 AWS_USE_SSL = False
 
 ALLOWED_HOSTS = ["*"]
+COMPRESS_OFFLINE = True
+COMPRESS_ENABLED = True
+
+print("Compress offline: ", COMPRESS_OFFLINE)
+print("Compress enabled: ", COMPRESS_ENABLED)
+
+STORAGES["staticfiles"] = {
+    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+}
--- config/production.py
+++ config/production.py
@@ -11,3 +11,52 @@ geoluminate.setup(development=False)
 STORAGES["staticfiles"] = {
     "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
 }
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            # "format": "{levelname} {asctime} {module}:{funcName} {message}",
+            "format": "{levelname} {asctime} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",  # Adjust the level as needed
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        # "file": {
+        #     "level": "INFO",  # Adjust the level as needed
+        #     "class": "logging.FileHandler",
+        #     "filename": os.path.join(BASE_DIR, "logs", "debug.log"),
+        #     "formatter": "verbose",
+        # },
+        # "mail_admins": {"level": "ERROR", "class": "django.utils.log.AdminEmailHandler", "formatter": "simple"},
+    },
+    "loggers": {
+        # "django": {
+        #     # "handlers": ["console", "file"],
+        #     "handlers": ["console"],
+        #     "level": "INFO",  # Adjust the level as needed
+        #     "propagate": True,
+        # },
+        "geoluminate": {  # Replace with the name of your Django app
+            "handlers": ["console"],
+            "level": "DEBUG",  # Adjust the level as needed
+            "propagate": False,
+        },
+        # "django.request": {
+        #     "handlers": ["mail_admins"],
+        #     "level": "ERROR",
+        #     "propagate": True,
+        # },
+    },
+}
