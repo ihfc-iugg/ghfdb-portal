f8fb7bc1433d96404c3c7a040d5ea2ba6b797c21
Sam <samuel.scott.jennings@gmail.com>
2024-03-28 12:11:43 +0100

    update demo

--- config/production.py
+++ config/production.py
@@ -1,17 +1,15 @@
-import geoluminate
-
 from .common import *
 from .geoluminate import *
 
 # need to fix this in geoluminate package
 SHOW_DEBUG_TOOLBAR = False
 
-geoluminate.setup(development=False)
 
 STORAGES["staticfiles"] = {
     "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
 }
 
+GEOLUMINATE_LABELS["sample"] = {"verbose_name": "Heat Flow Site", "verbose_name_plural": "Heat Flow Sites"}
 
 # STATIC_URL = "/django-static/"
 # COMPRESS_URL = STATIC_URL
