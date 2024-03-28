667aebe77fb7c3e65940da47be38c71751b1ae43
Sam <samuel.scott.jennings@gmail.com>
2024-03-28 12:58:53 +0100

    update production settings

--- config/production.py
+++ config/production.py
@@ -1,9 +1,11 @@
+import geoluminate
+
 from .common import *
 from .geoluminate import *
 
 # need to fix this in geoluminate package
 SHOW_DEBUG_TOOLBAR = False
-
+geoluminate.setup(development=False)
 
 STORAGES["staticfiles"] = {
     "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
