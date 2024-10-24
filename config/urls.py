f2dc4203362f55763a776a61017c54ada5c2cf78
Sam <samuel.scott.jennings@gmail.com>
2024-10-24 18:47:05 +0200

    add mapping app

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", include("geoluminate.urls")),
     path("", include("earth_science.location.urls")),
+    path("map/", TemplateView.as_view(template_name="home.html"), name="map"),
 ]
