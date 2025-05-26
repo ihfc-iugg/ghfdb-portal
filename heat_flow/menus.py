e94974bf68908d19b3ea1710fdf587d290be1a75
Sam <samuel.scott.jennings@gmail.com>
2025-05-26 09:28:47 +0200

    add new explore view

--- heat_flow/menus.py
+++ heat_flow/menus.py
@@ -1,31 +1,14 @@
 from django.utils.translation import gettext as _
 from fairdm.menus import DatabaseMenu
-from flex_menu import Menu, MenuItem
+from flex_menu import MenuItem
 
 DatabaseMenu.insert(
     [
-        Menu(
-            "MapMenu",
-            template="fairdm/menus/database/menu.html",
-            label=None,
-            children=[
-                # MenuItem(_("Global Heat Flow Database"), view_name="map", icon="map"),
-                MenuItem(
-                    _("Global Heat Flow Database"),
-                    url="https://worldheatflowdatabase.github.io/HeatFlowMapping/",
-                    icon="map",
-                ),
-            ],
+        MenuItem(
+            _("Explore"),
+            view_name="ghfdb-explore",
+            icon="map",
         ),
     ],
-    position=0,
-)
-
-
-DatabaseMenu.get("DatabaseMoreMenu").insert(
-    [
-        MenuItem(_("International Heat Flow Commission"), url="https://ihfc-iugg.org", icon="globe"),
-        MenuItem(_("HeatFlow.world"), url="https://heatflow.world", icon="globe"),
-    ],
-    position=0,
+    position=1,
 )
