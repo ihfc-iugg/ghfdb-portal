3ab59643c788dfb1661bc4d293c44f6d6892910b
Sam <samuel.scott.jennings@gmail.com>
2025-06-20 15:32:57 +0200

    init heat_flow

---
+++ project/heat_flow/factories.py
@@ -0,0 +1,116 @@
+import random
+
+import factory
+from factory.fuzzy import FuzzyChoice
+from fairdm.factories import MeasurementFactory
+from fairdm_geo.factories.location import BoreholeFactory, GeoDepthIntervalFactory
+
+from .models import (
+    # ChildConductivity,
+    HeatFlow,
+    HeatFlowInterval,
+    HeatFlowSite,
+    SurfaceHeatFlow,
+)
+
+
+class HeatFlowSiteFactory(BoreholeFactory):
+    environment = FuzzyChoice(HeatFlowSite.environment_vocab.values)
+    explo_method = FuzzyChoice(HeatFlowSite.explo_method_vocab.values)
+    explo_purpose = FuzzyChoice(HeatFlowSite.explo_purpose_vocab.values)
+
+    class Meta:
+        model = HeatFlowSite
+
+
+class HeatFlowIntervalFactory(GeoDepthIntervalFactory):
+    class Meta:
+        model = HeatFlowInterval
+
+
+class SurfaceHeatFlowFactory(MeasurementFactory):
+    class Meta:
+        model = SurfaceHeatFlow
+
+    value = factory.LazyAttribute(lambda _: round(random.gauss(mu=50, sigma=30), 2))
+    uncertainty = factory.LazyAttribute(lambda o: o.value * random.uniform(0.05, 0.25))  # noqa: S311
+    corr_HP_flag = FuzzyChoice([True, False, None])
+    is_ghfdb = factory.Faker("boolean", chance_of_getting_true=0.9)
+    # children = factory.RelatedFactoryList(
+    #     "heat_flow.factories.HeatFlowFactory",
+    #     parent=None,
+    #     factory_related_name="parent",
+    #     size=randint(1, 5),
+    # )
+
+
+class HeatFlowFactory(MeasurementFactory):
+    class Meta:
+        model = HeatFlow
+
+    # parent = factory.RelatedFactory(
+    #     "heat_flow.factories.HeatFlowFactory",
+    #     factory_related_name="relevant_child",
+    # )
+
+    value = factory.LazyAttribute(lambda _: round(random.gauss(mu=50, sigma=30), 2))
+    uncertainty = factory.LazyAttribute(lambda o: o.value * random.uniform(0.05, 0.25))  # noqa: S311
+
+    # metadata fields
+    method = FuzzyChoice(HeatFlow.method_vocab.values)
+    expedition = factory.Faker("text", max_nb_chars=100)
+    relevant_child = factory.Faker("boolean", chance_of_getting_true=0.8)
+
+    # probe fields
+    # length = factory.LazyAttribute(lambda o: random.uniform(0, o.probe_length))
+    probe_penetration = factory.Faker("pyfloat", min_value=0, max_value=1000)
+    probe_type = FuzzyChoice(HeatFlow.probe_type_vocab.values)
+    probe_length = factory.Faker("pyfloat", min_value=0, max_value=100)
+    probe_tilt = factory.Faker("pyfloat", min_value=0, max_value=90)
+    water_temperature = factory.Faker("pyfloat", min_value=-10, max_value=1000)
+
+    # temperature_gradient = factory.SubFactory("heat_flow.factories.TemperatureGradientFactory")
+    # thermal_conductivity = factory.SubFactory("heat_flow.factories.ChildConductivityFactory")
+
+    # correction fields
+    corr_IS_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_T_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_S_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_E_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_TOPO_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_PAL_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_SUR_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_CONV_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+    corr_HR_flag = FuzzyChoice(HeatFlow.corr_IS_flag_vocab.values)
+
+
+# class TemperatureGradientFactory(MeasurementFactory):
+#     class Meta:
+#         model = "heat_flow.TemperatureGradient"
+
+#     mean = factory.Faker("pyfloat", min_value=-(10**5), max_value=10**5)
+#     uncertainty = factory.Faker("pyfloat", min_value=0, max_value=10**5)
+#     corrected_mean = factory.Faker("pyfloat", min_value=-(10**5), max_value=10**5)
+#     corrected_uncertainty = factory.Faker("pyfloat", min_value=0, max_value=10**5)
+#     method_top = FuzzyChoice(TemperatureGradient.method_top_vocab.values)
+#     method_bottom = FuzzyChoice(TemperatureGradient.method_bottom_vocab.values)
+#     shutin_top = factory.Faker("pyfloat", min_value=0, max_value=10000)
+#     shutin_bottom = factory.Faker("pyfloat", min_value=0, max_value=10000)
+#     correction_top = FuzzyChoice(TemperatureGradient.correction_top_vocab.values)
+#     correction_bottom = FuzzyChoice(TemperatureGradient.correction_bottom_vocab.values)
+#     number = factory.Faker("pyint", min_value=1, max_value=100)
+
+
+# class ChildConductivityFactory(DjangoModelFactory):
+#     class Meta:
+#         model = "heat_flow.ChildConductivity"
+
+#     mean = factory.Faker("pyfloat", min_value=0, max_value=100)
+#     uncertainty = factory.Faker("pyfloat", min_value=0, max_value=100)
+#     source = FuzzyChoice(ChildConductivity.source_vocab.values)
+#     method = FuzzyChoice(ChildConductivity.method_vocab.values)
+#     saturation = FuzzyChoice(ChildConductivity.saturation_vocab.values)
+#     pT_conditions = FuzzyChoice(ChildConductivity.pT_conditions_vocab.values)
+#     pT_function = FuzzyChoice(ChildConductivity.pT_function_vocab, getter=lambda c: c[0])
+#     strategy = FuzzyChoice(ChildConductivity.strategy_vocab.values)
+#     number = factory.Faker("pyint", min_value=1, max_value=10000)
---
+++ project/heat_flow/models/__init__.py
@@ -0,0 +1,16 @@
+from .measurements import HeatFlow, IntervalConductivity, SurfaceHeatFlow, ThermalGradient
+from .samples import HeatFlowInterval, HeatFlowSite
+
+# from .vocabularies import ConductivitySource, ExplorationMethod, ExplorationPurpose
+
+__all__ = [
+    # "ConductivitySource",
+    # "ExplorationMethod",
+    # "ExplorationPurpose",
+    "HeatFlow",
+    "HeatFlowInterval",
+    "HeatFlowSite",
+    "IntervalConductivity",
+    "SurfaceHeatFlow",
+    "ThermalGradient",
+]
