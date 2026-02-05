0abe9e63a9e27d7eee5dc237d8b021ef56b912fe
Sam <samuel.scott.jennings@gmail.com>
2026-02-05 21:15:21 +0100

    refactor(heat_flow): remove SurfaceHeatFlow model
    Replaced by ghfdb.ParentHeatFlow. The heat_flow app now has no
    parent heat flow concept  it is purely scientific measurements.

--- project/heat_flow/models/__init__.py
+++ project/heat_flow/models/__init__.py
@@ -1,13 +1,10 @@
-from .measurements import HeatFlow, IntervalConductivity, SurfaceHeatFlow, ThermalGradient
+from .measurements import HeatFlow, IntervalConductivity, ThermalGradient
 from .samples import HeatFlowInterval, HeatFlowSite
-
-# from .vocabularies import ConductivitySource, ExplorationMethod, ExplorationPurpose
 
 __all__ = [
     "HeatFlow",
     "HeatFlowInterval",
     "HeatFlowSite",
     "IntervalConductivity",
-    "SurfaceHeatFlow",
     "ThermalGradient",
 ]
